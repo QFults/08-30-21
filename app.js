5ac9b87b455a6e8cbf3694e0b11262bb1f4404c4
Quinton Fults <quinton.fults.work@gmail.com>
2021-08-30 13:13:59 -0700

    code

--- app.js
+++ app.js
@@ -21,45 +21,49 @@
 
 // console.log(message, message1)
 
-const adder = (a, b) => {
-  return a + b
-}
+// const adder = (a, b) => {
+//   return a + b
+// }
 
-const subtracter = (a, b) => {
-  return a - b
-}
+// const subtracter = (a, b) => {
+//   return a - b
+// }
 
-const multiplier = (a, b) => {
-  return a * b
-}
+// const multiplier = (a, b) => {
+//   return a * b
+// }
 
-const divider = (a, b) => {
-  return a / b
-}
+// const divider = (a, b) => {
+//   return a / b
+// }
 
-const exponent = (a, b) => {
-  return a ** b
-}
+// const exponent = (a, b) => {
+//   return a ** b
+// }
 
-let [, , operation, num1, num2] = process.argv
+// let [, , operation, num1, num2] = process.argv
 
-num1 = parseInt(num1)
-num2 = parseInt(num2)
+// num1 = parseInt(num1)
+// num2 = parseInt(num2)
 
-switch (operation) {
-  case 'add':
-    console.log(adder(num1, num2))
-    break
-  case 'subtract':
-    console.log(subtracter(num1, num2))
-    break
-  case 'multiply':
-    console.log(multiplier(num1, num2))
-    break
-  case 'divide':
-    console.log(divider(num1, num2))
-    break
-  case 'exponent':
-    console.log(exponent(num1, num2))
-    break
-}
+// switch (operation) {
+//   case 'add':
+//     console.log(adder(num1, num2))
+//     break
+//   case 'subtract':
+//     console.log(subtracter(num1, num2))
+//     break
+//   case 'multiply':
+//     console.log(multiplier(num1, num2))
+//     break
+//   case 'divide':
+//     console.log(divider(num1, num2))
+//     break
+//   case 'exponent':
+//     console.log(exponent(num1, num2))
+//     break
+// }
+
+const movies = require('./movies.js')[0]
+
+console.log(movies)
---
+++ movies.js
@@ -0,0 +1,4 @@
+const movies = ['Goodfellas', 'The Room', 'movie3', 'movie4', 'movie5']
+const arr = []
+
+module.exports = [movies, arr]
